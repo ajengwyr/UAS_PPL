2f1308568f274f5fc6b114d681e43aeffa55c62c
ajengwyr <106131961+ajengwyr@users.noreply.github.com>
2024-02-06 10:06:37 +0700

    Update 5_click_profile_button_test.py

--- tests/5_click_profile_button_test.py
+++ tests/5_click_profile_button_test.py
@@ -6,8 +6,8 @@ class ProfileTestCase(unittest.TestCase):
 
     @classmethod
     def setUp(self):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
+        option = webdriver.FirefoxOptions()
+        option.add_argument('--headless')
         self.browser = webdriver.Firefox(options=option)
         try:
             self.url = os.environ['URL']
